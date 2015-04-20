b6f864ad0c3f6f96c61c25b815967d287ef82eb3
yangmengrong <yangwuan55@163.com>
2015-04-20 15:58:41 +0800

    init

---
+++ app/src/main/java/com/ymr/daofactory/DataSource.java
@@ -0,0 +1,10 @@
+package com.ymr.daofactory;
+
+import java.util.List;
+
+/**
+ * Created by ymr on 15/4/17.
+ */
+public interface DataSource {
+    List<Class> getDataClasses();
+}
---
+++ app/src/main/java/com/ymr/daofactory/test/db/Test2.java
@@ -0,0 +1,7 @@
+package com.ymr.daofactory.test.db;
+
+/**
+ * Created by ymr on 15/4/17.
+ */
+public class Test2 {
+}
